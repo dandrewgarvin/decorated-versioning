a74a8f6b63e4e60c373e3bbdf7a053dc17b379dc
Andrew Garvin <dandrewgarvin@gmail.com>
2018-02-23 19:49:07 -0700

    prepped for mac reboot

--- server/main.js
+++ server/main.js
@@ -1,5 +1,3 @@
-import { endianness } from 'os';
-
 const app = require('express')()
     , chalk = require('chalk')
     , port = 3005
@@ -16,4 +14,4 @@ setup(app)
 app.use('/api/v1', v1)
 app.use('/api/v2', v2)
 
-    app.listen(port, _=> console.log(`listening on port ${chalk.cyan(port)}`))
+app.listen(port, _=> console.log(`listening on port ${chalk.cyan(port)}`))
